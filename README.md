e7d08c6b50a54aabb33894b0a31085f60d9002b5
Egor Badmaev <badmaeve2511@gmail.com>
2020-11-14 11:43:49 +0300

    feat: 25.11 jc

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # My Python ЕГЭ tasks
-# Commits
-jc - just code - просто код. Идея, пока не решённое задание  
-ud - unproductive day - непродуктивный день. Ничего нового
--- zadanie_25_11.py
+++ zadanie_25_11.py
@@ -19,19 +19,24 @@ simple_counter = 0
 for i in range(25317, 51237 + 1):
     dividers = []
     simple_counter = 0
+    dividers_all = []
     for k in range(2, int(math.sqrt(i)) + 1):
         if i % k == 0:
             dividers_all.append(k)
             dividers_all.append(i // k)
-    for j in range(1, len(dividers_all)):
-        if simple_counter > 0:
-            break
+    for j in range(1, dividers_all):
         if dividers_all[j] % j == 0:
             simple_counter += 1
     if simple_counter == 0:
-        dividers.append(j)
-    if len(dividers) > 6:
-        print(dividers)
+    # for j in range(1, len(dividers_all)):
+    #     if simple_counter > 0:
+    #         break
+    #     if dividers_all[j] % j == 0:
+    #         simple_counter += 1
+    # if simple_counter == 0:
+    #     dividers.append(j)
+    # if len(dividers) > 6:
+    #     print(dividers)
 
 # """
 # Задание 25 (№506).
