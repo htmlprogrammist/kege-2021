a0c8baa3f5e944399e8afd512241b750ef819c1b
Egor Badmaev <badmaeve2511@gmail.com>
2020-11-15 10:46:03 +0300

    feat: 6.1 test, fix: trying to fix problems in 25.11

---
+++ zadanie_6_1.py
@@ -0,0 +1,15 @@
+s = int(input())
+n = 4
+
+while s < 37:
+    s += 3
+    n *= 2
+print(n)
+# result = 128
+# s = 0
+# n = 4
+# while n != result:
+#     while s < 37:
+#         s += 3
+#         n *= 2
+# print(s)
