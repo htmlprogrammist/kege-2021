c2c8e65cbe4f72465f47ab1fff1831fbbda7733a
Egor Badmaev <badmaeve2511@gmail.com>
2020-11-27 02:43:20 +0300

    feat: add 6.2, refactor: rename 6.1 from zadanie to task

---
+++ tasks_6/task_6_2.py
@@ -0,0 +1,19 @@
+s = 5
+k = 0
+counter = 0
+while k < 15:
+    k += 2
+    counter += 1
+    # s += k
+    s = s + k
+    print(counter, '-', s)
+print(s)  # 77
+
+# 1 - 7
+# 2 - 11
+# 3 - 17
+# 4 - 25
+# 5 - 35
+# 6 - 47
+# 7 - 61
+# 8 - 77
