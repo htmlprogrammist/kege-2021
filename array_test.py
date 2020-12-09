dc1ec53276073be2dc16d4ed6c0db5080218afdb
Egor Badmaev <badmaeve2511@gmail.com>
2020-12-09 10:43:07 +0300

    feat: OV homework 25.15-25.17, fix: 16.0 (added print), 16.2 - minor changes

---
+++ array_test.py
@@ -0,0 +1,8 @@
+A = []
+n = 10
+
+for i in range(n):
+    A.append(21)
+
+for j in range(n):
+    print(A[j], end=" ")
--- tasks_16/task_16.0-11.1.py
+++ tasks_16/task_16.0-11.1.py
@@ -3,3 +3,5 @@ def F(n):
     if n > 2:
         return F(n-1) + F(n-2)
     else: return n
+
+print(F(6))
