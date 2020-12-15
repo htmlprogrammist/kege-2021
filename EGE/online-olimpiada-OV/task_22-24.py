b82a8ee65829ec3e538c86e095859f5f7890f00d
Egor Badmaev <badmaeve2511@gmail.com>
2020-12-15 13:16:11 +0300

    feat: OV add solution to my first EGE try, 22-24.1

--- EGE/online-olimpiada-OV/task_22-24.py
+++ EGE/online-olimpiada-OV/task_22-24.py
@@ -8,7 +8,27 @@ AB, AA, AB, AD считаются
 
 document = open('22.txt')
 a = document.read()
+alphabet = []
+max_counter = 0
+max_counter_index = 0
+
+# chr[65:90]
+
+# Создаю массив с нулями для каждой буквы алфавита
+for j in range(26):
+    alphabet.append(0)
 
 for i in range(len(a) - 1):
-    if a[i] == 'A' and a[i + 1] == '':
-        pass
+    if a[i] == 'A':
+        symbol = ord(a[i + 1])  # Определяю символ
+        index = symbol - 65  # Место в алфавите
+        alphabet[index] += 1  # Счётчик, сколько раз
+# print(alphabet)
+
+for k in range(len(alphabet)):
+    if alphabet[k] > max_counter:
+        max_counter_index = k
+        max_counter = alphabet[k]
+print(chr(max_counter_index + 65))
+# print(max_counter == max(alphabet)) Проверка, совпадают ли значения максимума через функцию Питона
+# и то, как я нашёл максимум
