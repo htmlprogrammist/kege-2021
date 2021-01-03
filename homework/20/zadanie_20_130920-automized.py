c499f896178e8495e9f2d2cf0b1df03d7c80f94d
Egor Badmaev <badmaeve2511@gmail.com>
2021-01-03 17:34:34 +0300

    feat: add solution to homework/20.0 task and refactor: 20.1 (code convention indent)

--- homework/20/zadanie_20_130920-automized.py
+++ homework/20/zadanie_20_130920-automized.py
@@ -1,6 +1,7 @@
 end_of_interval = 200
 x = 1
 answer = 125
+
 while x <= end_of_interval:
     # x = int(input())
     L = 17
@@ -8,6 +9,6 @@ while x <= end_of_interval:
     while L <= M:
         L = L + 2*x
         M = M + x
-        if L == answer:
-            print(x)
+    if L == answer:
+        print(x)
     x += 1
