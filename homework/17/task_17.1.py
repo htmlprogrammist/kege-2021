6cb8f91b80d4197c9ba2cd4687cdc35fb8013055
Egor Badmaev <badmaeve2511@gmail.com>
2021-01-12 16:40:49 +0300

    delete: remove duplicate from the homework/17

--- homework/17/task_17.1.py
+++ homework/17/task_17.1.py
@@ -1,20 +1,18 @@
 """
-Рассматривается множество целых чисел, принадлежащих числовому отрезку [3712; 8432],
-которые удовлетворяют следующим условиям:
-− запись в двоичной и четверичной системах счисления заканчивается одинаковой цифрой;
-− кратны 13, 14 или 15.
-Найдите количество таких чисел и минимальное из них.
+Рассматривается множество целых чисел, принадлежащих числовому
+отрезку [4563; 7912], которые удовлетворяют следующим условиям:
+− число кратно 7;
+− сумма старшего и младшего разрядов больше 10.
+Найдите наибольшее из таких чисел и их количество.
 """
+counter = 0
+mx = 0
 
-for i in range(8432, 3712 + 1, -1):
-    current_binary_number_i = i
-    current_quaternary_number_i = i
-    binary_number_i = ''
-    quaternary_number_i = ''
-    while current_binary_number_i > 0:
-        binary_number_i = str(current_binary_number_i % 2) + binary_number_i
-        current_binary_number_i //= 2
-    while current_quaternary_number_i > 0:
-        quaternary_number_i = str(current_quaternary_number_i % 4) + quaternary_number_i
-        current_quaternary_number_i //= 4
-    print(i, '2', binary_number_i, '4', quaternary_number_i)
+for i in range(4563, 7912 + 1):
+    first = i // 1000
+    last = i % 10
+    if i % 7 == 0 and (first + last) > 10:
+        counter += 1
+        mx = i
+
+print(mx, counter)
