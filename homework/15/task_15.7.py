12b6adc39e8155d242069196f7cb94e7ce1f80c0
Egor Badmaev <badmaeve2511@gmail.com>
2021-01-14 16:18:01 +0300

    feat: add 15.7, w/o solution, because it is not possible

--- homework/15/task_15.7.py
+++ homework/15/task_15.7.py
@@ -1,5 +1,5 @@
 def F(x, y, A):
-    return ((x**2 + x - 20 >= 0) or check_set_A_X()) and ((x**2 - 3x - 18 <= 0) or check_set_A_X())
+    return ((x**2 + x - 20 >= 0) or check_set_A_X()) and ((x**2 - 3*x - 18 <= 0) or check_set_A_X())
 
 
 def check_set_A_X(set_x, A):
