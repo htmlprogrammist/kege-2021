3226d6456fd0cf31436a131301406fb7e2855638
Egor Badmaev <badmaeve2511@gmail.com>
2021-02-14 22:12:24 +0300

    feat: add solution to homework/2.8

--- homework/2/task_2_8.py
+++ homework/2/task_2_8.py
@@ -5,13 +5,13 @@ F = ((x → y) ∨ ¬ (z → w)) ∧ ((w → ¬x) ∨ (¬y → z)),
 даже не указав, какому столбцу таблицы соответствует каждая из переменных w, x, y, z.
 """
 print('x y z w F')
-r = False, True
+r = 0, 1
 for x in r:
     for y in r:
         for z in r:
             for w in r:
-                pass
+                # pass
                 # F = ((x <= y) or not(z <= w) and ((w <= not x) or (not y <= z)))
-                # F = (((x <= y) or not(z <= w)) and ((w <= not x) or (not y <= z)))
-                # if not F:
-                #     print(x, y, z, w, F)
+                F = (((x <= y) or not(z <= w)) and ((not w or not x) or (not y <= z)))
+                if not F:
+                    print(x, y, z, w, F)
