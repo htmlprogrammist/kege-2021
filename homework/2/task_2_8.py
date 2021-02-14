ef86ae41623624da99584ff0d13a87a9b167dcbb
Egor Badmaev <badmaeve2511@gmail.com>
2021-02-14 21:12:40 +0300

    feat: homework/2.5-2.8

---
+++ homework/2/task_2_8.py
@@ -0,0 +1,17 @@
+"""
+Задание 2 (№80).
+Вася успел заполнить лишь фрагмент таблицы истинности для выражения
+F = ((x → y) ∨ ¬ (z → w)) ∧ ((w → ¬x) ∨ (¬y → z)),
+даже не указав, какому столбцу таблицы соответствует каждая из переменных w, x, y, z.
+"""
+print('x y z w F')
+r = False, True
+for x in r:
+    for y in r:
+        for z in r:
+            for w in r:
+                pass
+                # F = ((x <= y) or not(z <= w) and ((w <= not x) or (not y <= z)))
+                # F = (((x <= y) or not(z <= w)) and ((w <= not x) or (not y <= z)))
+                # if not F:
+                #     print(x, y, z, w, F)
