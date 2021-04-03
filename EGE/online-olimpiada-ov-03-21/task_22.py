3393dc7e568f28b6f3e2a5921b1fcc0f4bd50659
Egor Badmaev <badmaeve2511@gmail.com>
2021-04-03 21:21:47 +0300

    refactor: rename files to its' normal naming (it is because statgrad suppose 19-21 as one task)

--- EGE/online-olimpiada-ov-03-21/task_22.py
+++ EGE/online-olimpiada-ov-03-21/task_22.py
@@ -1,49 +1,35 @@
-document = open('22.txt')
-a = document.readlines()
-string_indexes = []
-dictionary_a = {}
-list_of_n = []
-index = 0
-alphabet = [0] * 26
-min_k = 100
-min_k_index = 100
+"""
+Ниже на четырёх языках программирования записана программа,
+которая вводит натуральное число x,
+выполняет преобразования, а затем выводит два числа.
+Укажите наименьшее возможное значение x,
+при вводе которого программа выведет числа 3 и 11.
+"""
+x = int(input())
+k = x % 9
+a = 0
+b = 0
+while x > 0:
+    d = x % 9
+    if d == k:
+        a += 1
+    b += d
+    x //= 9
+print(a, b)
 
-
-for i in range(len(a)):
-    # a[i] = string
-    counter_n = a[i].count('N')
-    dictionary_a[i] = counter_n
-# print(dictionary_a)
-for key in dictionary_a:
-    list_of_n.append(dictionary_a[key])
-# print(list_of_n)
-min_amount_of_n = min(list_of_n)
-# print(min_amount_of_n)  # 23
-
-for key in dictionary_a:
-    if dictionary_a[key] == min_amount_of_n:
-        index = key
+number = 1
+while number < 1000:
+    x = number
+    k = x % 9
+    a = 0
+    b = 0
+    while x > 0:
+        d = x % 9
+        if d == k:
+            a += 1
+        b += d
+        x //= 9
+    if a == 3 and b == 11:
+        print(number)
         break
-# print(index)  # 764
-# print(string_indexes)  # [764, 849, 909], а после break мы имеем 764
-print(a[index])  # Строчка, с которой я сейчас работаю
-for i in range(len(a[index])):
-    symbol = ord(a[index][i])
-    s_index = symbol - 65
-    try:
-        alphabet[s_index] += 1
-    except IndexError:  # chr(10) ==> '\n', его надо обойти
-        # print(symbol, a[index][i], s_index)
-        pass
-
-# print(alphabet)
-
-for i in range(len(alphabet)):
-    if min_k > alphabet[i]:
-        min_k = alphabet[i]
-        min_k_index = i
-print(min_k_index)
-print(min_k, min(alphabet))
-min_k = min(alphabet)
-print(min_k)
-print(chr(min_k_index + 65))
+    number += 1
