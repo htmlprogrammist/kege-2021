fb6a8a3e2fb6aebbda878439447e578148bfc5e2
Egor Badmaev <badmaeve2511@gmail.com>
2021-04-05 19:53:21 +0300

    fix: add solution to 25.1

--- trouble_shooting/task_25_1.py
+++ trouble_shooting/task_25_1.py
@@ -5,11 +5,20 @@
 В ответе перечислите найденные числа в порядке возрастания
 (в отдельные поля для ответов).
 """
+from math import sqrt
 for i in range(45000000, 50000000 + 1):
-    counter_odd_del = 0
-    for j in range(2, i // 2 + 1):
+    counter_odd_del = 1
+    if int(sqrt(i))**2 == i and int(sqrt(i)) % 2 != 0:
+        counter_odd_del += 1
+    if i % 2 != 0:
+        counter_odd_del += 1
+    for j in range(2, int(sqrt(i))):
         if i % j == 0:
             if j % 2 != 0:
                 counter_odd_del += 1
+            if (i//j) % 2 != 0:
+                counter_odd_del += 1
+        if counter_odd_del > 5:
+            break
     if counter_odd_del == 5:
         print(i)
