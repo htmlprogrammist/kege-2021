4f0d5369195bdb1b6de61e087b7249e6e614fba1
htmlprogrammist <badmaeve2511@gmail.com>
2021-05-16 17:48:17 +0000

    feat: add solution to 16.10

---
+++ tasks_16/task_16_10.py
@@ -0,0 +1,21 @@
+from functools import *
+
+
+@lru_cache(None)
+def f(n):
+    if n <= 3:
+        return n
+    if n > 3:
+        if n % 2 == 0:
+            return f(n - 1) + 2 * f(n // 2)
+        else:
+            return f(n - 1) + f(n-3)
+
+
+counter = 0
+for n in range(67890):
+    result = f(n)
+    if result < 10 ** 8:
+        counter += 1
+
+print(counter)
