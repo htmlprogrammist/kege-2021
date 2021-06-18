ec714308cb4bd1ce6c7269c5e2c7acafe2afaaff
Egor Badmaev <badmaeve2511@gmail.com>
2021-06-18 19:37:37 +0300

    feat: add solution to 24.8

--- tasks_24/task_24_8.py
+++ tasks_24/task_24_8.py
@@ -6,13 +6,38 @@
 
 6
 """
-document = open('24/24_934.txt')
+f = open('24/24_934.txt')
+s = f.readline()
+# s = 'AABBAABBCCDDDEFFGF'
+single_string = s[0]  # накапливаем строку, где у нас будут все буквы разные
+count_letters = [1]  # накапливаем, сколько букв на каждой позиции соответствуют строке
+
+for i in range(1, len(s)):  # перебираем все символы с первого, т.к. первый уже есть
+    if s[i] == s[i - 1]:
+        count_letters[-1] += 1  # потому что это та же самая буква
+    else:
+        single_string += s[i]
+        count_letters.append(1)  # новая последовательностьо
+
+# print(single_string)
+# print(count_letters)
+max_len = 0
+
+for i in range(len(single_string) - 2):
+    if single_string[i] < single_string[i + 1] < single_string[i + 2]:
+        sum_letters = count_letters[i] + count_letters[i + 1] + count_letters[i + 2]
+        if max_len < sum_letters:
+            max_len = sum_letters
+
+print(max_len)
+
+# document = open('24/24_934.txt')
 # a = document.read()
-a = 'AABBAABBCCDDDEFFGF'
-k = 0
-max_k = 0
-symbols = set()
-prev = 0
+# a = 'AABBAABBCCDDDEFFGF'
+# k = 0
+# max_k = 0
+# symbols = set()
+# prev = 0
 
 # for i in range(len(a)):
 #     if prev <= ord(a[i]) and len(symbols) < 3:
@@ -30,23 +55,23 @@ prev = 0
 
 # он не сбрасывает значение для предыдущего элемента, поэтому решение с двумя циклами - не самая лучшая идея
 # или надо продумать как-то, чтобы он всё-таки научился его изменять
-for i in range(len(a) - 1):
-    # a[i] == prev
-    s = a[i]
-    for j in range(1, len(a)):
-        if prev <= ord(a[j]) and len(symbols) < 3:
-            k += 1
-            prev = ord(a[j])
-            if k > max_k:
-                max_k = k
-            if a[j] not in symbols:
-                symbols.add(a[j])
-            print(
-                'Symbols: {0}, k = {1}, max_k = {2}, cur_elem = {3}, previous = {4}'.format(symbols, k, max_k, a[j], a[i]))
-        else:
-            k = 1
-            symbols = set()
-            s = ''
-            prev = 0
-
-print(max_k)
+# for i in range(len(a) - 1):
+#     # a[i] == prev
+#     s = a[i]
+#     for j in range(1, len(a)):
+#         if prev <= ord(a[j]) and len(symbols) < 3:
+#             k += 1
+#             prev = ord(a[j])
+#             if k > max_k:
+#                 max_k = k
+#             if a[j] not in symbols:
+#                 symbols.add(a[j])
+#             print(
+#                 'Symbols: {0}, k = {1}, max_k = {2}, cur_elem = {3}, previous = {4}'.format(symbols, k, max_k, a[j], a[i]))
+#         else:
+#             k = 1
+#             symbols = set()
+#             s = ''
+#             prev = 0
+#
+# print(max_k)
